135fd3b14b0dcdbecba555de2235e1290ad428ba
lemonsaltytoo <v-lvkdl@naver.com>
2024-04-28 12:00:50 +0900

    Updated api rating

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,7 +9,7 @@ function Home() {
   const getMovie = async () => {
     const json = await (
       await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=7.4&sort_by=year"
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.0&sort_by=year"
       )
     ).json();
     setMovies(json.data.movies);
